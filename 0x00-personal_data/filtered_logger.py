e7cbbdc1d9d0140308658360272758f0fddb1ef5
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-02-08 20:49:36 +0200

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """filter datum function"""
 import re
 from typing import List
+import logging
 
 
 def filter_datum(
@@ -15,3 +16,22 @@ def filter_datum(
             message,  # no pep8
         )
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str] = [], separator: str = ";"):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+        self.separator = separator
+
+    def format(self, record: logging.LogRecord) -> str:
+        """filter datum"""
+        return filter_datum(
+            self.fields, self.REDACTION, super().format(record), self.separator
+        )
