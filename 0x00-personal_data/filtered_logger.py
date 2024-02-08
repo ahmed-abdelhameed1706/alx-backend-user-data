b44e3d2d88ded89ab2287bbf4997de6f2f47370b
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-02-08 21:18:52 +0200

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -54,9 +54,9 @@ def get_logger() -> logging.Logger:
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """returns a connector to the database"""
-    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
-    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
     user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
     db = os.getenv("PERSONAL_DATA_DB_NAME")
     try:
         connection = mysql.connector.connect(
@@ -68,3 +68,20 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
         return connection
     except mysql.connector.Error as e:
         return None
+
+
+def main() -> None:
+    """ " connects to db"""
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users")
+    logger = get_logger()
+    for row in cursor:
+        message = f"name={row[0]}; email={row[1]}; phone={row[2]}; ssn={row[3]}; password={row[4]}; ip={row[5]}; last_login={row[6]}; user_agent={row[7]};"
+        logger.info(message)
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
