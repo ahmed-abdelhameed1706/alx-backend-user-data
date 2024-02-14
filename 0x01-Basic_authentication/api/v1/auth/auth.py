26f50af8ae15ee466dde9f33b801a860c9dadd1b
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-02-14 02:04:13 +0200

    auth

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""auth class"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Auth class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """reqire auth method"""
+        if path is None:
+            return True
+        if excluded_paths is None or excluded_paths == []:
+            return True
+        if not path.endswith("/"):
+            path = path + "/"
+
+        if path in excluded_paths:
+            return False
+        else:
+            return True
+
+    def authorization_header(self, request=None) -> str:
+        """authorization header method"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar("User"):  # type: ignore
+        """current user method"""
+        return None
