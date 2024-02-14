a5ad8e1d65a9fd7581bfabb6d5c48d65deae8424
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-02-14 02:53:53 +0200

    task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 """basic auth"""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """basic auth class"""
 
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """extracting method for base64"""
         if authorization_header is None:
             return None
         if not isinstance(authorization_header, str):
@@ -15,3 +17,19 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.split(" ")[1]
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """decoding method for base64"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded = base64.b64decode(base64_authorization_header)
+
+        except Exception:
+            return None
+
+        return decoded.decode("utf-8")
