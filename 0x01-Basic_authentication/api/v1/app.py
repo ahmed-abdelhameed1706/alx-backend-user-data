b33b4e9687804de416442e5d8a3c2f772b122286
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-02-14 02:33:37 +0200

    adding task 5

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,11 +7,17 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import CORS, cross_origin
 import os
+from typing import Any
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+if getenv("AUTH_TYPE"):
+    from api.v1.auth.auth import Auth
+
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -30,6 +36,23 @@ def unauthorized(error) -> str:
 def forbidden(error) -> str:
     """Forbidden handler"""
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def handle_before_request():
+    """before request function"""
+    if auth is None:
+        return
+    paths = ["/api/v1/status/", "/api/v1/unauthorized/", "/api/v1/forbidden/"]
+
+    if not auth.require_auth(request.path, paths):
+        return
+
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 if __name__ == "__main__":
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -23,7 +23,12 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """authorization header method"""
-        return None
+        if request is None:
+            return None
+        if "Authorization" not in request.headers:
+            return None
+
+        return request.headers["Authorization"]
 
     def current_user(self, request=None) -> TypeVar("User"):  # type: ignore
         """current user method"""
