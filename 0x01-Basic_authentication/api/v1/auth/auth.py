588bb9d660423f94bd2c2f4e869d04a6daf063ca
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-02-15 11:05:22 +0200

    adding advanced2

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,13 +13,12 @@ class Auth:
             return True
         if excluded_paths is None or excluded_paths == []:
             return True
-        if not path.endswith("/"):
-            path = path + "/"
+        for excluded in excluded_paths:
+            if excluded.endswith("*"):
+                if path.startswith(excluded[:-1]) or path == excluded[:-1]:
+                    return False
 
-        if path in excluded_paths:
-            return False
-        else:
-            return True
+        return True
 
     def authorization_header(self, request=None) -> str:
         """authorization header method"""
@@ -28,7 +27,7 @@ class Auth:
         if "Authorization" not in request.headers:
             return None
 
-        return request.headers["Authorization"]
+        return request.headers.get("Authorization", None)
 
     def current_user(self, request=None) -> TypeVar("User"):  # type: ignore
         """current user method"""
