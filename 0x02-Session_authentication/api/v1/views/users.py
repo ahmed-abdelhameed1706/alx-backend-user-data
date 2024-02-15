f2ffdfa61544fee60d40ea454f5042651b581ef2
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-02-15 17:12:02 +0200

    fixing users

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -30,7 +30,7 @@ def view_one_user(user_id: str = None) -> str:
     if user_id == "me" and request.current_user is None:
         abort(404)
     if user_id == "me" and request.current_user is not None:
-        user_id = request.current_user.id
+        return jsonify(request.current_user.to_json())
 
     user = User.get(user_id)
     if user is None:
