06406ea5579c22e04ca153eec442218f8e8368e8
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-02-21 23:22:16 +0200

    auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -64,3 +64,8 @@ class Auth:
             return user
         except Exception:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """destroy session"""
+        self._db.update_user(user_id, session_id=None)
+        return None
