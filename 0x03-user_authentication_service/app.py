bd5f05737742b3afc3e26a960229799087af64b4
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-02-21 23:24:55 +0200

    app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """flask app"""
-from flask import Flask, jsonify, request, abort, make_response
+from flask import Flask, jsonify, request, abort, make_response, redirect, url_for
 from auth import Auth
 
 
@@ -38,6 +38,17 @@ def login() -> str:
     response = make_response(jsonify({"email": email, "message": "logged in"}))
     response.set_cookie("session_id", session_id)
     return response
+
+
+@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
+def logout() -> str:
+    """logout route"""
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
