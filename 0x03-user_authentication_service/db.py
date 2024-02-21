5abb24d0a242186b79d0b3f298a2e4d062d1fb94
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-02-21 21:40:34 +0200

    db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -2,7 +2,8 @@
 """DB module
 """
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
