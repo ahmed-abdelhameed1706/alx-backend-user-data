639f7ddd9cd8e289ead6ac082603a10c19450791
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-02-21 23:21:29 +0200

    auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -54,3 +54,13 @@ class Auth:
             return session_id
         except Exception:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> str:
+        """get user from session id"""
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except Exception:
+            return None
